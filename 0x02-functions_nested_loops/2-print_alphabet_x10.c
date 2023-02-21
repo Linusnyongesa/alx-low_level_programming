5b48221b2ca16daaa08d1aab90542e58a502f546
Linusnyongesa <linusnyongesa@gmail.com>
2023-02-21 14:36:37 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,10 +6,14 @@
  * Return: Always 0 (Succes)
  */
 void print_alphabet(void)
-{
-	int n;
 
-	for (n = 'a'; n <= 'z'; n++)
-		_putchar(n);
-	_putchar('\n');
+{
+	int m, n;
+
+	for (m = 0; m < 10; m++)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+			_putchar(n);
+		_putchar('\n');
+	}
 }
