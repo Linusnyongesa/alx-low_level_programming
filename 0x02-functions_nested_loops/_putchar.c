c303a943f62800935b3453590e0ffdce0350554c
Linusnyongesa <linusnyongesa@gmail.com>
2023-02-23 00:11:37 -0800

    2_main.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -5,10 +5,10 @@
  *
  * return: On success 1.
  * on error, -1 is returned, and errno is set appropriately
+ *
  */
 int _putchar(char c)
 {
-        return (write(1, &c, 1));
+	return (write(1, &c, 1));
 
 }
-
