42f4bc264122383a592577db91e0b55a6da6d593
Linusnyongesa <linusnyongesa@gmail.com>
2023-04-04 14:45:25 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -14,15 +14,15 @@
 
 #include <stdlib.h>
 
+typedef struct listint_s /*struct listint_s - singly linked list*/
 /*
- *  * struct listint_s - singly linked list
+ * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
  * for Holberton project
  */
-typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
